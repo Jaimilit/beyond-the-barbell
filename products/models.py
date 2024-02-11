1b76d12c42dbdffa7b6938c3bf8e8480fadbfa34
Jaimi Litus <Jaimilit@gmail.com>
2024-02-11 14:05:39 +0000

    added subscription model and added to categories and products for subscription

--- products/models.py
+++ products/models.py
@@ -3,6 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -22,6 +26,19 @@ class Product(models.Model):
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    subscription = models.ForeignKey('Subscription', null=True, blank=True, on_delete=models.SET_NULL)
+
+
+    def __str__(self):
+        return self.name
+    
+
+class Subscription(models.Model):
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    duration_months = models.IntegerField(default=1)
+    # You can add more fields specific to subscriptions
 
     def __str__(self):
         return self.name
