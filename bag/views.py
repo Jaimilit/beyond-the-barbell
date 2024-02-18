078bcf55103339960f674e85c2bd3b560f3f2d1d
Jaimi Litus <Jaimilit@gmail.com>
2024-02-18 10:40:00 +0000

    fixed error on bag.html page for calculating subtotal

--- bag/views.py
+++ bag/views.py
@@ -7,7 +7,6 @@ def view_bag(request):
 
     return render(request, 'bag/bag.html')
 
-
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
@@ -34,7 +33,7 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
-
+    
 
 def adjust_bag(request, item_id):
     """Adjust the quantity of the specified product to the specified amount"""
