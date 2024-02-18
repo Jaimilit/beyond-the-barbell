70e817f496a4962b2a540f3c3b31561a39b526c7
Jaimi Litus <Jaimilit@gmail.com>
2024-02-18 12:29:05 +0000

    added checkout views & templates

--- beyond_the_barbell/settings.py
+++ beyond_the_barbell/settings.py
@@ -49,6 +49,9 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+
+    # Other
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -62,6 +65,9 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'beyond_the_barbell.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 
 TEMPLATES = [
     {
@@ -77,8 +83,13 @@ TEMPLATES = [
                 'django.template.context_processors.request', # required 
                 'django.contrib.auth.context_processors.auth',  
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+             'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+             ]
         },
     },
 ]
