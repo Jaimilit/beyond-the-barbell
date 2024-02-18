d469612491edd5d0e6f5c6a2622ec9e48ea0b2bc
Jaimi Litus <Jaimilit@gmail.com>
2024-02-18 12:53:29 +0000

    add stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,9 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51OebP8KyQPzSAZYCUNoqfshZSK1asZjlxomcNufB0LS56drjeoL1usZcaTVD0aEnbDUlVvaGjQA7t5u6cL4mxjpA00w9t6joy2',
+        'client_secret': 'test client secret',
+
     }
 
     return render(request, template, context)
