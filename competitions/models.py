074c3e36183396278cdf22fbf50cdb591159dbb7
Jaimi Litus <Jaimilit@gmail.com>
2024-02-19 17:08:03 +0000

    start writing logic for model to book competitions

--- competitions/models.py
+++ competitions/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Competition(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='competitions/images/')
+
+class Booking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    competition = models.ForeignKey(Competition, on_delete=models.CASCADE)
+    date_booked = models.DateTimeField(auto_now_add=True)
--- competitions/views.py
+++ competitions/views.py
@@ -1,10 +1,29 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Competition, Booking
+from .forms import BookingForm
 
 # Create your views here.
-
 
 def competitions(request):
     """ A view to return the competitions page """
 
+
     return render(request, 'competitions.html')
+
+
+
+def book_competition(request, competition_id):
+    competition = Competition.objects.get(id=competition_id)
+    
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            booking = form.save(commit=False)
+            booking.user = request.user
+            booking.competition = competition
+            booking.save()
+            return redirect('booking_success')
+    else:
+        form = BookingForm()
+        
+    return render(request, 'book_competition.html', {'form': form, 'competition': competition})
