ba823998440b61cff08de20e8ecbf48a7decb42c
Jaimi Litus <Jaimilit@gmail.com>
2024-02-19 14:29:00 +0000

    removed secret key and set debug

--- beyond_the_barbell/settings.py
+++ beyond_the_barbell/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d$c8itugp+ip=-i+3ea$!cpxf78sd=g#u-8=7v5%97hlay2(u$'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     '8000-jaimilit-beyondthebarbe-soa2wsirv6t.ws-eu108.gitpod.io',
