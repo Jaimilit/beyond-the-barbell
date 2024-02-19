66eeec47c2d4ad52ca7f99efd7077c365651f6c9
Jaimi Litus <Jaimilit@gmail.com>
2024-02-19 14:51:10 +0000

    added env.py file as heroku app wasn't deploying

--- beyond_the_barbell/settings.py
+++ beyond_the_barbell/settings.py
@@ -14,6 +14,10 @@ from pathlib import Path
 import os
 import dj_database_url
 
+import os
+if os.path.exists("env.py"):
+  import env
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
