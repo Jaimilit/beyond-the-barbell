d580400be2f77308b2fa2f48edd1f29286344543
Jaimi Litus <Jaimilit@gmail.com>
2024-02-19 14:21:24 +0000

    deploy to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn beyond_the_barebell.wsgi:application
--- beyond_the_barbell/settings.py
+++ beyond_the_barbell/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -29,6 +30,9 @@ DEBUG = True
 ALLOWED_HOSTS = [
     '8000-jaimilit-beyondthebarbe-soa2wsirv6t.ws-eu108.gitpod.io',
     '8000-jaimilit-beyondthebarbe-soa2wsirv6t.ws-us108.gitpod.io',
+    'https://beyond-the-barbell-0789d667f147.herokuapp.com/',
+    'jaimilit-beyond-the-barbell.herokuapp.com/',
+    'localhost',
 ]
 
 
@@ -125,13 +129,24 @@ WSGI_APPLICATION = 'beyond_the_barbell.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+#DATABASES = {
+ #   'default': {
+  #      'ENGINE': 'django.db.backends.sqlite3',
+   #     'NAME': BASE_DIR / 'db.sqlite3',
+    #}
+#}
 
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
