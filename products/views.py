79f465630717792e4324f92ddda118f0711c217f
Jaimi Litus <Jaimilit@gmail.com>
2024-02-19 12:25:47 +0000

    added form for add product for admin

--- products/views.py
+++ products/views.py
@@ -3,6 +3,8 @@ from django.contrib import messages
 from django.db.models import Q
 from .models import Product, Category
 from django.db.models.functions import Lower
+from .forms import ProductForm
+
 
 
 # Create your views here.
@@ -67,3 +69,14 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
+
+
+def add_product(request):
+    """ Add a product to the store """
+    form = ProductForm()
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
