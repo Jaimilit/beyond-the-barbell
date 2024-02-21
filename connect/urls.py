dcbdf8d81e231a18696292515925ff94a81b3548
Jaimi Litus <Jaimilit@gmail.com>
2024-02-21 16:19:40 +0000

    reviews now show, but form is not working correctly

--- connect/urls.py
+++ connect/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('contact/', views.contact, name='contact'),
-    #path('reviews/', views.reviews, name='reviews'),
+    path('reviews/', views.reviews, name='reviews'),
     path('newsletter/', views.newsletter, name='newsletter'),
     path('private_policy/', views.private_policy, name='private_policy'),
     path('submit_review/', views.submit_review, name='submit_review'),
