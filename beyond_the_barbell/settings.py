2d6bf73eb3d31d4bd7ca8bd73c50a89312c9f52a
Jaimi Litus <Jaimilit@gmail.com>
2024-02-23 09:18:34 +0000

    change stripe email vertifaction

--- beyond_the_barbell/settings.py
+++ beyond_the_barbell/settings.py
@@ -29,8 +29,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = 'DEVELOPMENT' in os.environ
 DEBUG = 'DEVELOPMENT' in os.environ
+#DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-jaimilit-beyondthebarbe-soa2wsirv6t.ws-eu108.gitpod.io',
