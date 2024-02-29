bf32e42aec4aecf3a8d1f58703d00aec7ce0b679
Jaimi Litus <Jaimilit@gmail.com>
2024-02-29 19:48:37 +0000

    change reviews page to buttons show only if you are the user logged in

--- connect/views.py
+++ connect/views.py
@@ -10,7 +10,6 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseForbidden
 
 
-
 def contact(request, *args, **kwargs):
     """ A view to return the contact page """
     if request.method == 'POST':
@@ -48,39 +47,6 @@ def submit_review(request):
     else:
         form = ReviewForm()
     return render(request, 'submit_review.html', {'form': form})
-
-    """
-@login_required
-def submit_review(request):
-    if request.method == 'POST':
-        form = ReviewForm(request.POST)
-        if form.is_valid():
-            review = form.save(commit=False)
-            review.user = request.user
-            review.save()
-            messages.success(request, 'Thank you for your review!')
-            return redirect(reverse('reviews'))
-        else:
-            messages.error(request, 'Oops! There is an error with your review. Please check your details and try again.')
-    else:
-        form = ReviewForm()
-    return render(request, 'submit_review.html', {'form': form})
-
-    @login_required
-def edit_review(request, review_id):
-    review = get_object_or_404(TrainerReview, pk=review_id)
-    if request.method == 'POST':
-        form = ReviewForm(request.POST, instance=review)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Your review has been updated successfully.')
-            return redirect(reverse('reviews'))
-    else:
-        form = ReviewForm(instance=review)
-    return render(request, 'submit_review.html', {'form': form})
-"""
-
-
 
 
 @login_required
@@ -127,39 +93,6 @@ def reviews(request):
     reviews = TrainerReview.objects.all() 
     context = {'reviews': reviews}
     return render(request, 'reviews.html', context)
-
-
-"""
-
-@login_required
-def delete_review(request, review_id):
-    # Retrieve the review object
-    review = get_object_or_404(TrainerReview, pk=review_id)
-    
-    # Check if the logged-in user is the author of the review
-    if review.user_profile.user != request.user:
-        # If not the author, return an error or handle it as needed
-        return HttpResponseForbidden("You are not authorized to delete this review.")
-
-    if request.method == 'POST':
-        # If a POST request is received, proceed with deletion
-        review.delete()
-        messages.success(request, 'Your review has been deleted successfully.')
-    else:
-        # If not a POST request, return to the reviews page
-        messages.error(request, 'Invalid request method.')
-    return redirect('reviews')  # Redirect to the reviews page after deletion
-
-@login_required
-def delete_review(request, review_id):
-    review = get_object_or_404(TrainerReview, pk=review_id, user_profile=request.user.userprofile)
-    if request.method == 'POST':
-        review.delete()
-        messages.success(request, 'Your review has been deleted successfully.')
-        return redirect('reviews')
-    return render(request, 'delete_review.html', {'review': review})
-"""
-
 
 
 def newsletter(request):
