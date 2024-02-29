a68453849491d951822907a84b9ced165685a5d7
Jaimi Litus <Jaimilit@gmail.com>
2024-02-29 18:51:29 +0000

    change message email and debug

--- beyond_the_barbell/settings.py
+++ beyond_the_barbell/settings.py
@@ -32,8 +32,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
+#DEBUG = True
 #DEBUG = False
 
 ALLOWED_HOSTS = [
